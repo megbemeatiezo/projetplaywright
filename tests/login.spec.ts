9f810ffac04a221a108118744f7d8db688656abf
Atiezo <megbeme.atiezo@gmail.com>
2026-05-18 01:00:18 +0200

    Fix TypeScript errors

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,4 +1,6 @@
 import { test } from '@playwright/test';
+import * as allure from 'allure-js-commons';
+
 import { LoginPage } from '../pages/LoginPage';
 import { DashBoardPage } from '../pages/DashBoardPage';
 
@@ -6,27 +8,45 @@ test.describe('Login Tests', () => {
 
   test('Successful login', async ({ page }) => {
 
+    // Description visible dans Allure
+    await allure.description(
+      'Validation du login utilisateur et accès au dashboard'
+    );
+
+    // Tags
+    await allure.tags('login', 'smoke');
+
+    // Sévérité
+    await allure.severity('critical');
+
     // Instanciation des pages
     const loginPage = new LoginPage(page);
     const dashboardPage = new DashBoardPage(page);
 
-    // Ouvrir la page login
-    await loginPage.gotoLoginPage();
+    await allure.step('Ouvrir la page login', async () => {
+      await loginPage.gotoLoginPage();
+    });
 
-    // Vérifier que la page login est chargée
-    await loginPage.verifyLoginPageLoaded();
+    await allure.step('Vérifier que la page login est chargée', async () => {
+      await loginPage.verifyLoginPageLoaded();
+    });
 
-    // Connexion
-    await loginPage.login('Admin', 'admin123');
+    await allure.step('Connexion utilisateur', async () => {
+      await loginPage.login('Admin', 'admin123');
+    });
 
-    // Vérifications dashboard
-    await dashboardPage.verifyDashboardLoaded();
+    await allure.step('Vérifier chargement dashboard', async () => {
+      await dashboardPage.verifyDashboardLoaded();
+    });
 
-    // Vérification utilisateur connecté
-    await dashboardPage.verifyUserConnected();
+    await allure.step('Vérifier utilisateur connecté', async () => {
+      await dashboardPage.verifyUserConnected();
+    });
 
-    // Vérification URL
-    await dashboardPage.verifyUrlContains('dashboard');
+    await allure.step('Vérifier URL dashboard', async () => {
+      await dashboardPage.verifyUrlContains('dashboard');
+    });
+
   });
 
 });
